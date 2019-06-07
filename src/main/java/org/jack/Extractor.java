facddf7efc4913952fa1648e07fb0e3c5beab585
hal <hal@hal.com>
2019-06-07 19:45:19 +0100

    Now using csvWriter(list) rather than resultSet, to improve testability and pagination. Using orderBy

--- src/main/java/org/jack/Extractor.java
+++ src/main/java/org/jack/Extractor.java
@@ -2,13 +2,21 @@ package org.jack;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
 @Slf4j
 public class Extractor {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException, SQLException {
 
+        Properties properties = loadProperties();
 
-        try (  CSVExporter csvExporter = new CSVExporter() ) {
+        Repository repository = new Repository( properties );
+
+        try (  CSVExporter csvExporter = new CSVExporter( properties, repository ) ) {
 
             log.info("Starting csv exporter...");
 
@@ -22,6 +30,15 @@ public class Extractor {
 
     }
 
+    private static Properties loadProperties() throws IOException {
+        Properties properties = new Properties();
+
+        InputStream inputStream = Extractor.class.getClassLoader().getResourceAsStream("data.extractor.properties");
+
+        properties.load( inputStream );
+
+        return properties;
+    }
 
 
 }
